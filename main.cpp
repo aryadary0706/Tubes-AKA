0209f24506033910947727d895749c90763f806f
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2024-12-17 08:23:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,34 @@
-#include <iostream>
-#include "nama_pemain.h"
-using namespace std;
+#include "header.h"
 
-void searchPlayers(Player data[], int n, string posisi, int minRating, int maxRating, int minPrice, int maxPrice) {
-    bool found = false;
-
-    cout << "\nHasil pencarian pemain:\n";
-    for (int i = 0; i < n; i++) {
-        if (data[i].posisi == posisi && data[i].overall >= minRating && data[i].overall <= maxRating && data[i].price >= maxPrice && data[i].price <= maxPrice) {
-            cout << "- " << data[i].nama << endl;
-            found = true;
+int main(){
+    int num = 0;
+    Players Database;
+    while (num >= 0 && num <= 5){
+        cout<<"Pilih Database yang mau dipakai:\n";
+        cout<<"Data_Pemain1 (50)\n";
+        cin>>num;
+        if (num == 1) {
+        int N = 50;
+        init_players_1(Database);
+        
+        cout<<"Masukkan Data Pemain (position, Overall, value)"<<endl;
+        string cache_posisi;
+        int cache_Overall, cache_value;
+        cout<<"Masukkan posisi: "
+        cin>>cache_posisi
+        cout<<endl;
+        cout<<"Masukkan overall: "
+        cin>>cache_Overall
+        cout<<endl;
+        cout<<"Masukkan value: "
+        cin>>cache_value
+        cout<<endl;
+        
+        findPlayerIterative(Database, position, Overall, value, 50)
+        resetPlayers(Database, N);
+        }else if (num == 2){
+        
         }
     }
-
-    if (!found) {
-        cout << "Tidak ada pemain yang memenuhi kriteria.\n";
-    }
-}
-
-int main() {
-    string posisi;
-    int minRating, minPrice;
-    int maxRating, maxPrice;
-
-    cout << "Masukkan posisi yang dicari (contoh: ST, GK, CM): ";
-    cin >> posisi;
-    cout << "Masukkan rating minimal pemain: ";
-    cin >> minRating;
-    cout << "Masukkan rating maximal pemain: ";
-    cin >> maxRating;
-    cout << "Masukkan harga minimal pemain: ";
-    cin >> minPrice;
-    cout << "Masukkan harga maksimal pemain: ";
-    cin >> maxPrice;
-
-    searchPlayers(Dataplayers, N, posisi, minRating,maxRating, minPrice, maxRating);
-
-    return 0;
+    
 }
