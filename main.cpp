fc5f705d276e9859020582e1649312b7e5fd7079
dewakkl <maladewaadrian12@gmail.com>
2024-12-18 15:32:53 +0700

    Update main.cpp
    nanti ku perbaiki

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
-#include <iostream>
+#include "tubes.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    Players Data;
+    int N = 2; // Jumlah pemain yang diinisialisasi
+
+    // Inisialisasi data pemain
+    init_players_1(Data);
+
+    // Input pencarian
+    string searchPosition;
+    int searchOverall, searchValue;
+
+    cout << "Masukkan posisi pemain yang dicari (contoh: ST): ";
+    cin >> searchPosition;
+
+    cout << "Masukkan overall pemain yang dicari (79-91): ";
+    cin >> searchOverall;
+
+    cout << "Masukkan nilai transfer pemain yang dicari: ";
+    cin >> searchValue;
+
+    // Pencarian pemain
+    string result = findPlayer(Data, searchPosition, searchOverall, searchValue, N, searchValue * 0.1);
+
+    // Output hasil pencarian
+    if (result != "None") {
+        cout << "Pemain ditemukan: " << result << endl;
+    } else {
+        cout << "Pemain tidak ditemukan." << endl;
+    }
+
     return 0;
 }
