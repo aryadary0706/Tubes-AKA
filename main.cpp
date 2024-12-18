5ab921ddcb494bf29236cccd6fc85c9f168af1b5
dewakkl <maladewaadrian12@gmail.com>
2024-12-18 15:29:32 +0700

    Update main.cpp hari ini

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "header.h"
 
 int main(){
+    /*
     int num = 0;
     Players Database;
     while (num >= 0 && num <= 5){
@@ -10,7 +11,7 @@ int main(){
         if (num == 1) {
         int N = 50;
         init_players_1(Database);
-        
+
         cout<<"Masukkan Data Pemain (position, Overall, value)"<<endl;
         string cache_posisi;
         int cache_Overall, cache_value;
@@ -23,12 +24,62 @@ int main(){
         cout<<"Masukkan value: "
         cin>>cache_value
         cout<<endl;
-        
+
         findPlayerIterative(Database, position, Overall, value, 50)
         resetPlayers(Database, N);
         }else if (num == 2){
-        
+
         }
     }
-    
+
+
+    init_players_1(Data);
+    string SearchedPlayers;
+    SearchedPlayers = findPlayerIterative(Data, "ST", 91, 180000000, 50);
+
+    // Input pencarian pemain
+    string searchPosition;
+    int searchOverall, searchValue;
+
+    cout << "Masukkan posisi pemain yang dicari (contoh: ST): ";
+    cin >> searchPosition;
+
+    cout << "Masukkan overall pemain yang dicari (79-91): ";
+    cin >> searchOverall;
+
+    cout << "Masukkan nilai transfer pemain yang dicari: ";
+    cin >> searchValue;
+    */
+
+    Players Data;
+    int N = 50;
+
+    // Inisialisasi data pemain
+    init_players_1(Data);
+
+    // Input pencarian
+    string searchPosition;
+    int searchOverall, searchValue;
+
+    cout << "Masukkan posisi pemain yang dicari (contoh: ST): ";
+    cin >> searchPosition;
+
+    cout << "Masukkan overall pemain yang dicari (79-91): ";
+    cin >> searchOverall;
+
+    cout << "Masukkan nilai transfer pemain yang dicari: ";
+    cin >> searchValue;
+
+    // Pencarian pemain
+    string result = findPlayerIterative(Data, searchPosition, searchOverall, searchValue, N);
+
+    // Output hasil pencarian
+    if (result != "None") {
+        cout << "Pemain ditemukan: " << result << endl;
+    } else {
+        cout << "Pemain tidak ditemukan." << endl;
+    }
+
+    return 0;
 }
+
